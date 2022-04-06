e6df8d648a89f86e600598d16c830564b75ac2e1
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-04-06 17:21:32 -0300

    feat: start application from app.R
    Changes in styling.
    Instantiate shinyApp and call it from runApp.
    Use environment variable PORT.

--- app.R
+++ app.R
@@ -1,7 +1,14 @@
 require("shiny")
+
 ui <- fluidPage(
   "Hello, world!"
 )
 server <- function(input, output, session) {
 }
-shinyApp(ui, server)
+
+app <- shinyApp(ui, server)
+
+port_env <- as.numeric(Sys.getenv("PORT"))
+port <- ifelse(port_env == "", 3500, port_env)
+
+runApp(app, port = port)
