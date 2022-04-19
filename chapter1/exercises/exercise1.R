0a1168e7441d070f49537013533731d7e3285d93
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-04-19 01:28:09 -0300

    fix: change output format

--- chapter1/exercises/exercise1.R
+++ chapter1/exercises/exercise1.R
@@ -3,18 +3,19 @@ require("shiny")
 ui <- fluidPage(
   textInput("name", "What's your name?"),
   numericInput("age", "How old are you?", value = NA, min = 0),
-  textOutput("greeting", inline = FALSE),
+  textOutput("greeting1", inline = FALSE),
+  textOutput("greeting2", inline = FALSE)
 )
 
 server <- function(input, output, session) {
-  output$greeting <- renderText({
-    paste0(c(
-      "Hello. My name is ",
-      input$name,
-      " and I’m ",
-      input$age,
-      " years old."
-    ))
+  output$greeting1 <- renderText({
+    stringr::str_interp(
+      "Hello. My name is ${input$name}, and I’m ${input$age} years old."
+    )
+  })
+
+  output$greeting2 <- renderText({
+    stringr::str_interp("Hello, ${input$name}. It’s a pleasure to meet you.")
   })
 }
 
