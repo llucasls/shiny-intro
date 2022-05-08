97bd0c5e608ec3f195b0ac35508a536000bb64c8
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-05-08 16:03:55 -0300

    feat: create three new user inputs

--- chapter2/app.R
+++ chapter2/app.R
@@ -1,1 +1,7 @@
 require("shiny")
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  passwordInput("password", "What's your password?"),
+  textAreaInput("story", "Tell me about yourself", rows = 3)
+)
