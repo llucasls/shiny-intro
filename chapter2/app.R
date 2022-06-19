a23c07ab3072fae0deda8c31e0450b9b0d9a4031
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-06-19 12:13:03 -0300

    feat: add numeric inputs

--- chapter2/app.R
+++ chapter2/app.R
@@ -3,7 +3,10 @@ require("shiny")
 ui <- fluidPage(
   textInput("name", "What's your name?"),
   passwordInput("password", "What's your password?"),
-  textAreaInput("story", "Tell me about yourself", rows = 3)
+  textAreaInput("story", "Tell me about yourself", rows = 3),
+  numericInput("num", "Number one", value = 0, min = 0, max = 100),
+  sliderInput("num2", "Number two", value = 50, min = 0, max = 100),
+  sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100)
 )
 
 server <- function(input, output, session) {
