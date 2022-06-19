3e4219b91a5a147f2c42540b0821f15b15678e5d
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-06-19 12:19:18 -0300

    feat: add date inputs

--- chapter2/app.R
+++ chapter2/app.R
@@ -6,7 +6,9 @@ ui <- fluidPage(
   textAreaInput("story", "Tell me about yourself", rows = 3),
   numericInput("num", "Number one", value = 0, min = 0, max = 100),
   sliderInput("num2", "Number two", value = 50, min = 0, max = 100),
-  sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100)
+  sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100),
+  dateInput("dob", "When were you born?"),
+  dateRangeInput("holiday", "When do you want to go on vacation next?")
 )
 
 server <- function(input, output, session) {
