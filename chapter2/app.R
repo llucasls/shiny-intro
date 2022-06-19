99735d95a5f4fa1ac409755073681012c0f55434
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-06-19 12:49:25 -0300

    feat: add section titles

--- chapter2/app.R
+++ chapter2/app.R
@@ -1,12 +1,16 @@
 require("shiny")
 
 ui <- fluidPage(
+  titlePanel(title = "Basic UI", windowTitle = "Basic UI"),
+  markdown("### Example 1: Free Text"),
   textInput("name", "What's your name?"),
   passwordInput("password", "What's your password?"),
   textAreaInput("story", "Tell me about yourself", rows = 3),
+  markdown("### Example 2: Numeric Inputs"),
   numericInput("num", "Number one", value = 0, min = 0, max = 100),
   sliderInput("num2", "Number two", value = 50, min = 0, max = 100),
   sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100),
+  markdown("### Example 3: Dates"),
   dateInput("dob", "When were you born?"),
   dateRangeInput("holiday", "When do you want to go on vacation next?")
 )
