33b8ea73a4b9ff41cb3c478a5cf9577884075b86
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-09-08 15:28:25 -0300

    refactor: separate examples in lists

--- chapter2/app.R
+++ chapter2/app.R
@@ -1,19 +1,28 @@
 require("shiny")
 
-ui <- fluidPage(
-  titlePanel(title = "Basic UI", windowTitle = "Basic UI"),
+title <- titlePanel(title = "Basic UI", windowTitle = "Basic UI")
+
+example1 <- list(
   markdown("### Example 1: Free Text"),
   textInput("name", "What's your name?"),
   passwordInput("password", "What's your password?"),
-  textAreaInput("story", "Tell me about yourself", rows = 3),
+  textAreaInput("story", "Tell me about yourself", rows = 3)
+)
+
+example2 <- list(
   markdown("### Example 2: Numeric Inputs"),
   numericInput("num", "Number one", value = 0, min = 0, max = 100),
   sliderInput("num2", "Number two", value = 50, min = 0, max = 100),
-  sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100),
+  sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100)
+)
+
+example3 <- list(
   markdown("### Example 3: Dates"),
   dateInput("dob", "When were you born?"),
   dateRangeInput("holiday", "When do you want to go on vacation next?")
 )
+
+ui <- fluidPage(title, example1, example2, example3)
 
 server <- function(input, output, session) {
 }
